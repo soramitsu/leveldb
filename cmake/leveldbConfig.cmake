08598f4d5395fed630ad5174d19ace53afe89c65
Paweł Bylica <chfast@gmail.com>
2019-05-28 15:27:51 +0200

    Add dependencies to the leveldbConfig.cmake

--- cmake/leveldbConfig.cmake
+++ cmake/leveldbConfig.cmake
@@ -1,1 +1,7 @@
+include(CMakeFindDependencyMacro)
+
+find_dependency(Threads)
+find_dependency(crc32c CONFIG REQUIRED)
+find_dependency(Snappy CONFIG REQUIRED)
+
 include("${CMAKE_CURRENT_LIST_DIR}/leveldbTargets.cmake")
